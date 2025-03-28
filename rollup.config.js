f929ca92948f42c3c899a0f8764f39c326760b9b
Fernando Salom <fer@rudo.es>
2025-03-28 12:05:37 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+// rollup.config.js
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import json from '@rollup/plugin-json'; // 👈 Añade esto
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    json(), // 👈 Y este plugin aquí
+    typescript({ useTsconfigDeclarationDir: true }),
+  ],
+};
